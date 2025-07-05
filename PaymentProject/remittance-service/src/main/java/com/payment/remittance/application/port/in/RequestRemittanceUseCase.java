017a9237a019c4f07827a565051371523cd02ddd
김형우 <ksd5653@naver.com>
2025-07-05 18:35:58 +0900

    Revert "Reapply "FEAT - 송금 서비스 구현""
    This reverts commit 7fec95315a657f417701668cb93cf30c84c540f5.

--- PaymentProject/remittance-service/src/main/java/com/payment/remittance/application/port/in/RequestRemittanceUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.payment.remittance.application.port.in;
-
-
-import com.payment.remittance.domain.RemittanceRequest;
-
-public interface RequestRemittanceUseCase {
-    RemittanceRequest requestRemittance(RequestRemittanceCommand command);
-}
--- PaymentProject/remittance-service/src/main/java/com/payment/remittance/application/port/out/membership/MembershipPort.java
+++
@@ -1,6 +0,0 @@
-package com.payment.remittance.application.port.out.membership;
-
-public interface MembershipPort {
-
-    MembershipStatus getMembershipStatus(String membershipId);
-}
